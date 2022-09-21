236880076439f41dd94afd069a8cf4a4dcee2e22
hwang152 <lanse1521@163.com>
2022-09-21 21:32:43 +0800

    异常日志处理 commons-web的异常日志处理，此模块并未引入日志依赖，但可以使用日志 原因：这个模块并不是直接运行，没有main方法，而是将里面的代码整合到了可以运行的stu-server模块运行

--- commons/commons-web/src/main/java/com/qf/commons/web/exception/GlobalExceptionHandler.java
+++ commons/commons-web/src/main/java/com/qf/commons/web/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.qf.commons.web.exception;
 import com.qf.commons.core.exception.ServiceException;
 import com.qf.commons.data.result.R;
 import com.qf.commons.data.result.RCodes;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,18 +14,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  */
 //RestController的增强
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ServiceException.class)
     public R serviceExceptionHandler(ServiceException e){
-        System.out.println("发生了全局的业务异常"+e.getMessage());
         return R.createFail(e.getCode(),e.getMsg());
     }
 
     @ExceptionHandler()
     public R globExceptionHandler(Throwable t) {
         //捕获异常 日志记录当前异常信息
-        System.out.println("发生了全局异常!" + t.getMessage());
+//        System.out.println("发生了全局异常!" + t.getMessage());
+        log.error("全局异常被捕获！",t);
         return R.createFail(RCodes.FAIL);
     }
 }
