f7d468fda013c791a454bc56d50b4947d6b5556a
hwang152 <lanse1521@163.com>
2022-10-10 16:28:43 +0800

    认证服务创建 auth-server

---
+++ ability/ability-atuh/src/main/java/com/qf/ability/auth/application/AuthApplication.java
@@ -0,0 +1,15 @@
+package com.qf.ability.auth.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author 14526
+ * create_time 2022/10/10
+ */
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
