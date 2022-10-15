0d47267f6d8b1017f71f1b8d7ac7a6dc0b4e09e4
hwang152 <lanse1521@163.com>
2022-10-15 22:37:45 +0800

    elasticSearch - term、match、multi_match、range、bool复合、排序、高亮查询

--- ability/ability-search/src/test/java/com/qf/ability/test/SearchTestApplication.java
+++ ability/ability-search/src/test/java/com/qf/ability/test/SearchTestApplication.java
@@ -3,18 +3,21 @@ package com.qf.ability.test;
 import com.qf.ability.search.application.SearchApplication;
 import com.qf.ability.test.entity.Goods;
 import com.qf.ability.test.service.IGoodsService;
-import org.elasticsearch.index.query.MultiMatchQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.RangeQueryBuilder;
+import org.elasticsearch.index.query.*;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.HighlightQuery;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.data.elasticsearch.core.query.Query;
+import org.springframework.util.CollectionUtils;
 
 import java.util.Date;
 import java.util.List;
@@ -77,10 +80,33 @@ public class SearchTestApplication {
                 .field("title",20)
                 .field("info",1);
         //range查询
-        RangeQueryBuilder rangeQuery = QueryBuilders.rangeQuery("price").gte(1000).lte(3000);
+        RangeQueryBuilder price = QueryBuilders.rangeQuery("price").gte(1000).lte(3000);
+
+        //bool查询 复合查询 查询价钱小于1000的洗衣机 大于2000的电视机
+        BoolQueryBuilder boolQuery = QueryBuilders.boolQuery()
+                .should(QueryBuilders.boolQuery()
+                        .must(QueryBuilders.matchQuery("title", "洗衣机"))
+                        .must(QueryBuilders.rangeQuery("price").lte(1000)))
+                .should(QueryBuilders.boolQuery()
+                        .must(QueryBuilders.matchQuery("title", "电视机"))
+                        .must(QueryBuilders.rangeQuery("price").gte(2000)));
+
+        //查询全部
+        MatchAllQueryBuilder matchAllQuery = QueryBuilders.matchAllQuery();
 
         //创建一个查询对象
-        Query query = new NativeSearchQuery(rangeQuery);
+        NativeSearchQuery query = new NativeSearchQuery(matchQuery);
+
+        //排序 执行查询
+        query.addSort(Sort.by(Sort.Order.asc("price")));
+        //添加高亮
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.field("title").preTags("<font color='red'>").postTags("</font>");
+        query.setHighlightQuery(new HighlightQuery(highlightBuilder));
+
+        //创建一个查询对象
+        //Query query = new NativeSearchQuery(boolQuery);
+
         //term搜索 通过实体类判断去那个索引库搜索
         SearchHits<Goods> hits = template.search(query, Goods.class);
         //获得搜索的结果列表
@@ -89,7 +115,13 @@ public class SearchTestApplication {
         System.out.println("搜索的结果数量："+hits.getTotalHits());
         for (SearchHit<Goods> searchHit : searchHits) {
             //SearchHit -> 表示一个查询结果记录 （Document）
-            System.out.println(searchHit.getContent());
+            Goods goods = searchHit.getContent();
+            //获取高亮
+            List<String> highlight = searchHit.getHighlightField("title");
+            if (!CollectionUtils.isEmpty(highlight)) {
+                goods.setTitle(highlight.get(0));
+            }
+            System.out.println(goods);
         }
     }
 }
