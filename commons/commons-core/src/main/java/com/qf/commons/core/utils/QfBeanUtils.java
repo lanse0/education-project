06d054fba24af17e6f023948640bf7a61ac109e7
hwang152 <lanse1521@163.com>
2023-03-15 19:58:39 +0800

    urils - 封装转换集合内对象类型的工具

--- commons/commons-core/src/main/java/com/qf/commons/core/utils/QfBeanUtils.java
+++ commons/commons-core/src/main/java/com/qf/commons/core/utils/QfBeanUtils.java
@@ -3,6 +3,10 @@ package com.qf.commons.core.utils;
 import com.qf.commons.core.exception.ServiceException;
 import org.springframework.beans.BeanUtils;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 /**
  * 工具类 把任意一个个对象转换成为想要的对象  vo -> entity
  * author 14526
@@ -18,5 +22,17 @@ public class QfBeanUtils {
         } catch (Exception e) {
             throw new ServiceException(500, tcls + "初始化异常");
         }
+    }
+
+    public static <T> List<T> copyList(Collection collection, Class<T> tcls) {
+        if (collection == null || collection.size() == 0) return null;
+        List<T> targetList = new ArrayList<>();
+        //循环集合
+        for (Object sourceObj : collection) {
+            //逐个拷贝
+            T targetObj = copyBean(sourceObj, tcls);
+            targetList.add(targetObj);
+        }
+        return targetList;
     }
 }
